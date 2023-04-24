26dc7a9b7c92829d7be1f2ef0f1867d0b4f7c2b7
Bunix25 <svattt7@gmail.com>
2023-04-24 13:47:41 -0400

    Updates

---
+++ precache-manifest.806cd3e352f8870c271ee96739d0620a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebf438688c8977069b34",
+    "url": "/React-Portfolio/static/css/main.0c826a0d.chunk.css"
+  },
+  {
+    "revision": "ebf438688c8977069b34",
+    "url": "/React-Portfolio/static/js/main.ebf43868.chunk.js"
+  },
+  {
+    "revision": "b965d220e0d6421fd4e0",
+    "url": "/React-Portfolio/static/js/1.b965d220.chunk.js"
+  },
+  {
+    "revision": "b995dddb165fed188421",
+    "url": "/React-Portfolio/static/js/2.b995dddb.chunk.js"
+  },
+  {
+    "revision": "6a22a35ab43c50aae954",
+    "url": "/React-Portfolio/static/js/runtime~main.6a22a35a.js"
+  },
+  {
+    "revision": "1ab6c3624f3f1409d9be671b77ca9e8a",
+    "url": "/React-Portfolio/static/media/Profile_pic.1ab6c362.jpg"
+  },
+  {
+    "revision": "c8cf80dbdac73911e08c024148bff1bd",
+    "url": "/React-Portfolio/static/media/Project2.c8cf80db.png"
+  },
+  {
+    "revision": "36ca52e375bf67c7e0288c030002f933",
+    "url": "/React-Portfolio/static/media/tech-blog.36ca52e3.png"
+  },
+  {
+    "revision": "a25d9ad7d88bc6c67e6bbabf3ed54e52",
+    "url": "/React-Portfolio/static/media/Weather_Dash.a25d9ad7.png"
+  },
+  {
+    "revision": "1d80f05fa74180924d9b034ab7cafc0f",
+    "url": "/React-Portfolio/static/media/NoteTaker.1d80f05f.png"
+  },
+  {
+    "revision": "cec9c1d6b20ef698677ec9e83b8255b0",
+    "url": "/React-Portfolio/static/media/Jate.cec9c1d6.png"
+  },
+  {
+    "revision": "fb1d705121e6197c96ff0ac4ed30d5c9",
+    "url": "/React-Portfolio/static/media/Vintage.fb1d7051.png"
+  },
+  {
+    "revision": "a684b2a33f68f0ad787dbb788a7744aa",
+    "url": "/React-Portfolio/static/media/team-profile-generator.a684b2a3.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/React-Portfolio/static/media/Resume_2022.d41d8cd9.pdf"
+  },
+  {
+    "revision": "576f665f4d8fd828126b53d056484a88",
+    "url": "/React-Portfolio/static/media/5330558.576f665f.jpg"
+  },
+  {
+    "revision": "7f05c17a2b6a3b0dd5f808a392da6b0d",
+    "url": "/React-Portfolio/index.html"
+  }
+];
